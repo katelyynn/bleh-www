78f6500bafa44052e476ddbf4416e31691f5d171
katelyn <46572320+katelyynn@users.noreply.github.com>
2026-02-13 01:59:38 +0000

    feat: more images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { IconBlocks, IconDownload, IconDroplet, IconHeart, IconProgressCheck, IconUsersGroup } from "@tabler/icons-react";
+import { IconBlocks, IconDownload, IconDroplet, IconHeart, IconMessage2, IconProgressCheck, IconUsersGroup } from "@tabler/icons-react";
 import { Brand } from "./_components/brand/brand";
 import { Card, Content, SideLeft, SideRight } from "./_components/content/content";
 import { Feature, FeatureIcon, FeatureList } from "./_components/feature/feature";
@@ -40,7 +40,7 @@ export default function Home() {
                 <Content column={false} outer>
                     <SideLeft span>
                         <Content>
-                            <p>image goes here</p>
+                            <Image src={'/themes.webp'} alt="Theming" />
                         </Content>
                     </SideLeft>
                     <SideRight>
@@ -83,18 +83,43 @@ export default function Home() {
                     <Content column={false} outer>
                         <SideLeft>
                             <Content>
-                                <Image src={'/close_friends.webp'} alt="Stay connected" shrink />
+                                <Image src={'/express.webp'} alt="Keep the conversation going" shrink />
                             </Content>
                         </SideLeft>
                         <SideRight span>
                             <Content>
-                                <FeatureIcon icon={IconUsersGroup} large />
+                                <FeatureIcon icon={IconMessage2} large />
                                 <h2>Keep the conversation going</h2>
-                                <p>Add people as <em>close friends</em> to keep updated on their recent listening.<br />Select one as a special <em>‘starred friend’</em> to view their scrobbles alongside yours everywhere.</p>
+                                <p>Express yourself with native <em>Markdown integration</em> in shoutboxes and profile descriptions, including <em>images, line breaks, timestamps, text formatting</em> etc.<br />Additionally, see <em>shoutbox previews</em> return on music pages to get a snippet of what’s being discussed at the moment.</p>
                             </Content>
                         </SideRight>
                     </Content>
                 </Card>
+            </Section>
+            <Section alternate>
+                <Content column={false} outer>
+                    <SideLeft span>
+                        <Content>
+                            <p>image goes here</p>
+                        </Content>
+                    </SideLeft>
+                    <SideRight>
+                        <Content>
+                            <h2>Sponsors get rewards</h2>
+                            <FeatureList>
+                                <Feature icon={IconDroplet} title="Choose your style">
+                                    Enjoy a range of five themes from the brightest to the darkest.<br />Find the colour that suits you best from a wide range of presets, seasonal events, and a customiser to fine-tune.
+                                </Feature>
+                                <Feature icon={IconBlocks} title="Built from experience">
+                                    Every page has been restructured based on my own experiences using Last.fm through the years, so hopefully it’ll feel better for you too.
+                                </Feature>
+                                <Feature icon={IconProgressCheck} title="Frequently updated">
+                                    The update checker helps you have a bug-free and feature-full time.<br />Changelogs are included to keep you in the loop on what’s happening.
+                                </Feature>
+                            </FeatureList>
+                        </Content>
+                    </SideRight>
+                </Content>
             </Section>
         </>
     )
