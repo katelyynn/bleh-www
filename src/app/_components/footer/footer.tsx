4db87c4812527476295b5d13ac036d28f7208169
katelyn <46572320+katelyynn@users.noreply.github.com>
2026-02-13 20:20:02 +0000

    chore: footer link

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export function Footer() {
     return (
         <section className={styles.footer}>
-            katelyn 2023-2026
+            katelyn <a href="https://github.com/katelyynn/bleh" target="_blank">2023-2026</a>
         </section>
     )
 }
